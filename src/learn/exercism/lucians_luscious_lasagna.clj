f87e855579d1dbe444e2430afd3dfe26e60f9f70
joaofsoares <joaofsoares@yahoo.com>
2025-06-06 12:59:27 +0100

    initial commit

---
+++ src/learn/exercism/lucians_luscious_lasagna.clj
@@ -0,0 +1,10 @@
+(ns learn.exercism.lucians-luscious-lasagna (:gen-class))
+
+(def expected-time 40)
+
+(defn remaining-time [actual-time] (- 40 actual-time))
+
+(defn prep-time [num-layers] (* num-layers 2))
+
+(defn total-time [num-layers actual-time] (+ (prep-time num-layers) actual-time))
+
