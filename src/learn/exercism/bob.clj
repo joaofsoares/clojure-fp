7cedf678e2c2d815549187339e25cea28fc81b5d
joaofsoares <joaofsoares@yahoo.com>
2025-06-08 20:50:15 +0100

    bob format

--- src/learn/exercism/bob.clj
+++ src/learn/exercism/bob.clj
@@ -1,17 +1,16 @@
 (ns learn.exercism.bob
   (:require [clojure.string :as str]))
 
-(defn isFilterEmpty? [s] (empty? (filter #(Character/isLetter %) s)))
+(defn- is-filter-empty? [s] (empty? (filter #(Character/isLetter %) s)))
 
-(defn isYelling? [s] (and (not (isFilterEmpty? s)) (every? #(Character/isUpperCase %) (filter #(Character/isLetter %) s))))
+(defn is-yelling? [s] (and (not (is-filter-empty? s)) (every? #(Character/isUpperCase %) (filter #(Character/isLetter %) s))))
 
-(defn isQuestion? [s] (= \? (last s)))
+(defn is-question? [s] (= \? (last s)))
 
 (defn response-for [s] (let [x (str/trim s)]
                          (cond
                            (empty? x)  "Fine. Be that way!"
-                           (and (not (isYelling? x)) (isQuestion? x)) "Sure."
-                           (and (isYelling? x) (isQuestion? x)) "Calm down, I know what I'm doing!"
-                           (and (isYelling? x) (not (isQuestion? x))) "Whoa, chill out!"
+                           (and (not (is-yelling? x)) (is-question? x)) "Sure."
+                           (and (is-yelling? x) (is-question? x)) "Calm down, I know what I'm doing!"
+                           (and (is-yelling? x) (not (is-question? x))) "Whoa, chill out!"
                            :else "Whatever.")))
-
